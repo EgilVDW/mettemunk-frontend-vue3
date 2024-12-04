5412e6281e5148042d0b91a87de7373ddb790525
Egil W <egil.wendsjo@gmail.com>
2024-12-04 13:41:38 +0100

    Fixed cardItemBlock padding

--- components/cardItemBlock/cardItemBlock.vue
+++ components/cardItemBlock/cardItemBlock.vue
@@ -7,7 +7,7 @@
 				:data="data.image.responsiveImage"
 				layout="responsive"
 			/>
-			<div v-if="isLargeScreen" class="default-card__text-content flex-vertical-direction flex-space-between">
+			<div v-if="!isMobile && isClient" class="default-card__text-content flex-vertical-direction flex-space-between">
 				<div>
 					<h2 class="h3 h3--light c-mb-l c-mt-reset">{{ data.title }}</h2>
 					<p class="text-default text-default--light c-mt-reset">{{ data.description }}</p>
@@ -38,7 +38,7 @@
 import type { ICardBlock } from "~/interfaces/blocks";
 import { useMediaQueryCustom } from "~/composable/useMediaQueryCustom";
 
-const { isLargeScreen } = useMediaQueryCustom();
+const { isMobile, isClient } = useMediaQueryCustom();
 
 defineProps({
 	data: {
--- composable/useMediaQueryCustom.ts
+++ composable/useMediaQueryCustom.ts
@@ -1,9 +1,19 @@
 import { useMediaQuery } from "@vueuse/core"
 export const useMediaQueryCustom = () => {
 
+    const isMobile = useMediaQuery('(max-width: 540px)')
+
     const isLargeScreen = useMediaQuery('(min-width: 922px)')
     
+    const isClient = ref(false);
+    
+    onMounted(() => {
+        isClient.value = true
+    })
+
     return {
+        isMobile,
         isLargeScreen,
+        isClient,
     }
 }
