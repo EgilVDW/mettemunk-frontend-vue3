86f063d0348ca1c40488edeef5fcd112a29a2d13
FlareRaxer <jonaskruse123@gmail.com>
2024-12-04 13:59:48 +0100

    added loader

---
+++ components/loader/loader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app-backdrop">
+        <div class="app-backdrop__logo">
+            <IconLogo />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    args: {
+        type: Boolean,
+        default: true,
+    },
+});
+</script>
